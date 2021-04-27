32f2483a1428247ef530eadbf9fb4c196a1f9f75
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-27 13:14:30 -0600

    Fix typo

--- src/contactTab.js
+++ src/contactTab.js
@@ -4,8 +4,8 @@ function contactTab() {
     tab.setAttribute('class', 'tab');
     let phone = document.createElement('p');
     let email = document.createElement('p');
-    phone.textContent = 'Phone Number: 2222 4444 / 800 800 FOOD';
-    email.textContent = 'le_siugpla_e_distos@gmail.com';
+    phone.textContent = 'Phone: 2222 4444 / 800 800 FOOD';
+    email.textContent = 'Email: le_siugpla_e_distos@gmail.com';
     tab.append(phone);
     tab.append(email);
     container.append(tab);
