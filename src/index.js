038ad1825d3add28f9d8f942831cc0c55ef6c1c7
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-27 12:50:30 -0600

    Import tabs and connect binders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('!');
+import pageLoad from './page-load.js';
+import descTab from './descTab.js';
+import menuTab from './menuTab.js';
+import contactTab from './contactTab.js';
+
+(function () {
+    // Preloads website base elements
+    pageLoad();
+    // Cache
+    let description = document.querySelector('#desc-tab');
+    let menu = document.querySelector('#menu-tab');
+    let contact = document.querySelector('#contact-tab');
+    let tabContent = document.querySelector('#tab-content');
+    // Binds
+    description.addEventListener('click', () => {
+        loadTab(descTab);
+    });
+    menu.addEventListener('click', function () {
+        loadTab(menuTab);
+    });
+    contact.addEventListener('click', function () {
+        loadTab(contactTab);
+    });
+    // Functions
+    function loadTab(tabFunction) {
+        tabContent.innerHTML = ''; // Deletes current contect
+        tabFunction();
+    }
+})();
