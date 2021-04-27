7acce5fbecca24b9687e5861c97f21e25ed334de
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-27 12:48:24 -0600

    Add contact tab function

---
+++ src/contactTab.js
@@ -0,0 +1,14 @@
+function contactTab() {
+    let container = document.querySelector('#tab-content');
+    let tab = document.createElement('div');
+    tab.setAttribute('class', 'tab');
+    let phone = document.createElement('p');
+    let email = document.createElement('p');
+    phone.textContent = 'Phone Number: 2222 4444 / 800 800 FOOD';
+    email.textContent = 'le_siugpla_e_distos@gmail.com';
+    tab.append(phone);
+    tab.append(email);
+    container.append(tab);
+}
+
+export default contactTab;
